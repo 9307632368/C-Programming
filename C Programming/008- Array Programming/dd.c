9d5a69384a561b2d8d802787b9e277bf8eae46f4
Rohan Sanjay Pol <120255126+9307632368@users.noreply.github.com>
2024-09-05 23:57:13 +0530

    Add files via upload

---
+++ C Programming/008- Array Programming/dd.c
@@ -0,0 +1,13 @@
+// run time array initialization
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a[5]={1,2,3,4,5};
+printf("Enter a 5 numbers: ");
+int i=0;
+// for( i=0;i<=5;i++){
+//     scanf("%d",&a[i]);
+// }
+printf("%d",a[i]);
+return 0;
+}
